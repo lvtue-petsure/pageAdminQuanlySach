cabb25dfa50d66b85a06edc30cd7681d15f4e191
lvtur <lvtue@tma.com.vn>
2025-09-19 16:52:47 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://xbyabttdnhlfjbtjpvjq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhieWFidHRkbmhsZmpidGpwdmpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4NjUwMzUsImV4cCI6MjA3MjQ0MTAzNX0.WWDvYbbJJiSS427VB4bvbRtvfF-uAHoCy4w6cewMveI';
+const SUPABASE_URL = 'https://btbxfgirzbvmqfhrltcy.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0YnhmZ2lyemJ2bXFmaHJsdGN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNjg2MzAsImV4cCI6MjA3Mzg0NDYzMH0.NxCBsFUCVA9kojfWaqogrMk4yI6wXW3q4JvKnHAaS9k';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
